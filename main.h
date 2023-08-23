61927ad03803f3f91aefbc9301da18bd5617c049
dennismutitu006 <dennismutitu06@gmail.com>
2023-08-23 01:24:48 -0700

    changes

--- main.h
+++ main.h
@@ -2,7 +2,10 @@
 #define MAIN_H
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdarg.h>
+<<<<<<< HEAD
+=======
+#include <unistd.h>
+>>>>>>> 1f39b9828e891b6b72938d37eef3879b3c510611
 int _printf(const char *format, ...);
 #endif
